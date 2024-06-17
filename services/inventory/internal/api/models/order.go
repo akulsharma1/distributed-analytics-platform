15cd4d786ed714f992f76519d7f0989c7a9163df
akulsharma1 <akulsharma02@gmail.com>
2024-06-17 14:38:34 -0700

    db struct adjustments

--- services/inventory/internal/api/models/order.go
+++ services/inventory/internal/api/models/order.go
@@ -4,20 +4,18 @@ import "gorm.io/gorm"
 
 type Order struct {
 	gorm.Model
-    ProductName string `gorm:"type:varchar(100);not null"`
 	CustomerEmail string `gorm:"type:varchar(100);not null;index"`
 	Customer    Customer `gorm:"foreignKey:CustomerEmail;references:Email"`
 
-	OrderItem []OrderItem
+	OrderItems []OrderItem
 }
 
 type OrderItem struct {
     gorm.Model
-    OrderID   uint   `gorm:"not null;index;primaryKey"` // Composite primary key
-    ProductID uint   `gorm:"not null;index;primaryKey"` // Composite primary key
-    Size      string `gorm:"type:varchar(100);not null;primaryKey"` // Composite primary key
+    OrderID   int   `gorm:"not null;index;primaryKey"` // Composite primary key
+    VariantID int   `gorm:"not null;index;primaryKey"` // Composite primary key
     Quantity  int    `gorm:"type:int;not null"`
 
     Order     Order  `gorm:"foreignKey:OrderID"` // Relation to Order
-    Variant   Variant `gorm:"foreignKey:ProductID,Size;references:ProductID,Size"` // Relation to Variant
+    Variant   Variant `gorm:"foreignKey:VariantID;references:id"` // Relation to Variant
 }
--- services/inventory/internal/api/models/product.go
+++ services/inventory/internal/api/models/product.go
@@ -10,8 +10,8 @@ type Product struct {
 
 type Variant struct {
     gorm.Model
-    ProductID uint    `gorm:"index:idx_product_size,unique;autoIncrement:false" json:"product_id"` // Part of the unique index
-    Size      string  `gorm:"index:idx_product_size,unique;type:varchar(100)" json:"size"` // Part of the unique index
+    ProductID int    `gorm:"type:int;not null" json:"product_id"` // Part of the unique index
+    Size      string  `gorm:"type:varchar(100);not null" json:"size"` // Part of the unique index
     Price     float64 `gorm:"type:decimal(10,2);not null" json:"price"`
 	
     Quantity  int     `gorm:"type:int;not null" json:"quantity"`
